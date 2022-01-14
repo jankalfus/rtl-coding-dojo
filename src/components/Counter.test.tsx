55a82cef81e2879b4cdf7f1f6d37aae155d49f8e
Jan Kalfus <JanK@kentico.com>
2022-01-14 19:20:49 +0100

    Add tests and implement Counter

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -1,9 +1,38 @@
-test.todo("Count is initially zero");
+import { render, screen } from "@testing-library/react";
+import { Counter } from "./Counter";
+import userEvent from "@testing-library/user-event";
 
-test.todo('Clicking the "+" button increments the counter');
+test("Count is initially zero", () => {
+  render(<Counter />);
 
-test.todo('Clicking the "-" button decrements the counter');
+  expect(screen.getByText(/0/)).toBeInTheDocument();
+});
 
-test.todo('Combining the "+" and "-" button clicks yields the correct value');
+test('Clicking the "+" button increments the counter', () => {
+  render(<Counter />);
 
-export {};
+  userEvent.click(screen.getByRole("button", { name: "+" }));
+  userEvent.click(screen.getByRole("button", { name: "+" }));
+
+  expect(screen.getByText(/2/)).toBeInTheDocument();
+});
+
+test('Clicking the "-" button decrements the counter', () => {
+  render(<Counter />);
+
+  userEvent.click(screen.getByRole("button", { name: "-" }));
+  userEvent.click(screen.getByRole("button", { name: "-" }));
+  userEvent.click(screen.getByRole("button", { name: "-" }));
+
+  expect(screen.getByText(/-3/)).toBeInTheDocument();
+});
+
+test('Combining the "+" and "-" button clicks yields the correct value', () => {
+  render(<Counter />);
+
+  userEvent.click(screen.getByRole("button", { name: "+" }));
+  userEvent.click(screen.getByRole("button", { name: "+" }));
+  userEvent.click(screen.getByRole("button", { name: "-" }));
+
+  expect(screen.getByText(/1/)).toBeInTheDocument();
+});
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
 import "./Counter.css";
 
 export function Counter() {
-  return null;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <div className="count">Count: {count}</div>
+      <div>
+        <button className="button" onClick={() => setCount((c) => c - 1)}>
+          -
+        </button>
+        <button className="button" onClick={() => setCount((c) => c + 1)}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
